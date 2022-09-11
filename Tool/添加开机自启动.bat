63e948a9aa4f6d1f0383d07de495fa33857f0543
Temperature6 <61113523+Temperature6@users.noreply.github.com>
2022-09-11 22:47:22 +0800

    Delete 添加开机自启动.bat

--- Tool/添加开机自启动.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-set "CurrentPath=!cd!"
-set "exeName=\ScreenShot.exe"
-set "exePath=%CurrentPath%%exeName%"
-start "" .\RegAutoRun.exe "-add" "ScreenShotBGRun" %exePath% -background
-pause
